91d901d7c95f1b4edf0295e6b577f48a9225fdf2
Dominykas <dominykas.ceskevicius@stud.vilniustech.lt>
2022-12-23 01:07:08 +0200

    new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Cars.csproj 	new file:   Controllers/CarController.cs 	new file:   DataModels/CarContext.cs 	new file:   Models/Car.cs 	new file:   Models/CarDTO.cs 	new file:   Program.cs 	new file:   Properties/launchSettings.json 	new file:   Querry/Cars_database.sql 	new file:   Services/CarService.cs 	new file:   appsettings.Development.json 	new file:   appsettings.json

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Software.Design.DataModels;
+using Software.Design.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CarContext>(options =>
+    options
+        .UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=")
+        .UseSnakeCaseNamingConvention());
+
+builder.Services.AddScoped<CarService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
